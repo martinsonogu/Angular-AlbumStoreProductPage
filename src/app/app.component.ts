ba3593e1fb8c6a2bd6a94c366e9ae98e00cf2719
onogumartins <onogumartins01@gmail.com>
2020-10-06 07:28:56 +0100

    Completed half module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ProductService } from "./product.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css", "ProductService"],
 })
 export class AppComponent {
-  title = 'app works!';
+  title = "app works!";
 }
