b8beb7b5dcbb2532da6ca8cea0a32a8bdce37784
onogumartins <onogumartins01@gmail.com>
2020-10-06 21:59:14 +0100

    Complete module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,23 +9,12 @@ import { ProductDescriptionComponent } from "./product-description/product-descr
 import { ProductService } from "./product.service";
 import { ProductTracklistingComponent } from "./product-tracklisting/product-tracklisting.component";
 import { ProductListComponent } from "./product-list/product-list.component";
-import { RouterModule } from "@angular/router";
-import { Routes } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 
 const appRoutes: Routes = [
-  {
-    path: products;
-  component: ProductListComponent
-},
-  {
-    path: product/:id;
-    component: ProductPageComponent;
-  },
-  {
-    path: '';
-    redirectTo: products;
-    pathMatch: full;
-  }
+  { path: "products", component: ProductListComponent },
+  { path: "product/:id", component: ProductPageComponent },
+  { path: "", redirectTo: "products", pathMatch: "full" },
 ];
 
 @NgModule({
@@ -36,7 +25,12 @@ const appRoutes: Routes = [
     ProductTracklistingComponent,
     ProductListComponent,
   ],
-  imports: [BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(appRoutes)],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+  ],
   providers: [ProductService],
   bootstrap: [AppComponent],
 })
