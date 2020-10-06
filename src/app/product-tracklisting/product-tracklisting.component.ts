0dc3712163dab14927aeb13f0191f883f0d18a93
onogumartins <onogumartins01@gmail.com>
2020-10-06 19:37:54 +0100

    Complete module

---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from "@angular/core";
+import { Album } from "../album";
+import { ProductService } from "../product.service";
+
+@Component({
+  selector: "app-product-tracklisting",
+  templateUrl: "./product-tracklisting.component.html",
+  styleUrls: ["./product-tracklisting.component.css"],
+})
+export class ProductTracklistingComponent implements OnInit {
+  albumInfo: Album;
+  constructor() {
+    _productservice;
+  }
+
+  ngOnInit() {
+    this._productservice
+      .getAlbum(1)
+      .subscribe((response) => (this.albumInfo = response));
+  }
+}
